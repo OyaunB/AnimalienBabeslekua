d54b4114dc2f18b6800ac5e53e2737b4e1d52f51
OyaunB <betxeberria24@izarraitz.eus>
2026-09-15 13:31:10 +0200

    Animalia klasea egiten....

--- src/main/java/org/example/Animalia.java
+++ src/main/java/org/example/Animalia.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Animalia {
+    private String Izena;
+    private int Adina;
+    private double Pisua;
+    private boolean Adoptatua;
+
+    //Eraikitzailea-Constructor__:
+    public  Animalia(String izena, int adina, double pisua){
+        Izena = izena;
+        Adina = adina;
+        Pisua = pisua;
+        Adoptatua = false;  // Hasieran ez dago adoptatuta
+    }
+
+    //Metodo abstraktua, beheko klase bakoitzak bere espeziea bueltatuko du (Katua,txakurra, untxia)
+    public abstract String Espeziea;
+
+    //Funtzioak=  ((ToString, adoptatu,, eginSoinua>>>
+    //Espeziea, adoptatua, adina, pisua, izena
+    //Herentzia: Untxia, Katua, Txakurra
+
 }
