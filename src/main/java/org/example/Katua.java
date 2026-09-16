a578bc003c6a37860e5f291888d4162ab6bc75e4
OyaunB <betxeberria24@izarraitz.eus>
2026-09-16 08:35:01 +0200

    Katua klasea eginda

--- src/main/java/org/example/Katua.java
+++ src/main/java/org/example/Katua.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class Katua {
+public class Katua extends Animalia {
+
+    public Katua(String izena, int adina, double pisua){
+        super(izena, adina, pisua); //Deitu gurasoen eraikitzaileari
+    }
+
+    @Override
+    public String  espeziea(){
+        return "Katua";
+    }
+
+    @Override
+    public void eginSoinua(){
+        System.out.println("Katuak miau egin du");
+    }
 }
